945295cdffd696f40a143e1e63ae79aa5f8c32bc
flavio.santana <9090flavio@gmail.com>
2023-11-10 12:17:10 -0300

    feat: Buscando dados da API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+
+    val httpClient: HttpClient = HttpClient.newHttpClient()
+
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+    val response = httpClient.send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+
+    println(json)
+
 }

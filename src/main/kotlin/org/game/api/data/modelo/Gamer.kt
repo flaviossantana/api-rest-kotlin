aa986b90c5b4bd1430213523ab6dc74a8c811eb4
flavio.santana <9090flavio@gmail.com>
2023-11-10 16:52:39 -0300

    feat: Adicionando novos métodos

--- src/main/kotlin/org/game/api/data/modelo/Gamer.kt
+++ src/main/kotlin/org/game/api/data/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package org.game.api.data.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome: String, var email: String) {
     var dataNascimento: String? = null
     var usuario: String? = null
@@ -9,7 +11,17 @@ data class Gamer(var nome: String, var email: String) {
             this(nome, email) {
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        gerarIdentificador()
     }
 
+    private fun gerarIdentificador() {
+        val sequencial = Random.nextInt(10000)
+        val tag = String.format("%04d", sequencial)
 
+        this.idInterno = "$usuario#$tag"
+    }
+
+    override fun toString(): String {
+        return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
+    }
 }
--- src/test/kotlin/org/game/api/data/modelo/GamerTest.kt
+++ src/test/kotlin/org/game/api/data/modelo/GamerTest.kt
@@ -4,22 +4,21 @@ package org.game.api.data.modelo
 fun main() {
     val jogadorN1 = Gamer("Jao Jogador da Silva", "jjs@email.com")
 
+    jogadorN1.let {
+        it.usuario = "jjs_vicio"
+    }
+
+    println(jogadorN1)
+
     val jogadorN2 = Gamer(
         "Maria Jogadora da Silva",
         "mjs@email.com",
         "01/09/2010",
-        "ID:12103546804651890"
+        "mariica"
     )
 
-    println(jogadorN1)
     println(jogadorN2)
 
-    jogadorN1.let {
-        it.usuario = "jjs_vicio"
-        it.idInterno = "ID:7112080808000"
-    }
-
-    println(jogadorN1)
 }
 
 

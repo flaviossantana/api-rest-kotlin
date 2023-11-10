650b481b3115adba97969ab8ce908cd967e31577
flavio.santana <9090flavio@gmail.com>
2023-11-10 12:34:54 -0300

    feat: Criando a classe Jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,10 @@
+class Jogo {
+    var titulo = ""
+    var capa  = ""
+    var descricao = ""
+
+    override fun toString(): String {
+        return "Jogo(titulo='$titulo', capa='$capa', descricao='$descricao')"
+    }
+
+}

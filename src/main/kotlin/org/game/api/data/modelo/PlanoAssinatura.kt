d08fae7628c823d54b1b581dfe41c56cf4b91c2d
flavio.santana <9090flavio@gmail.com>
2023-11-14 17:55:50 -0300

    feat: Implementando desconto para o plano assinatura

--- src/main/kotlin/org/game/api/data/modelo/PlanoAssinatura.kt
+++ src/main/kotlin/org/game/api/data/modelo/PlanoAssinatura.kt
@@ -3,16 +3,25 @@ package org.game.api.data.modelo
 class PlanoAssinatura(
     tipo: String,
     val mensalidade: Double,
-    val jogosInclusos: Int
+    val jogosInclusos: Int,
+    val descontoReputacao: Double
 ) : Plano(tipo) {
 
     override fun caucularAluguel(aluguel: Aluguel): Double {
+
         val totalJogosAlugadosMes = aluguel.gamer.jogosDoMes(aluguel.periodo.inicio.monthValue).size
 
         return if (jogosInclusos > totalJogosAlugadosMes) {
             0.0
         } else {
-            super.caucularAluguel(aluguel)
+
+            var valorAluguel = super.caucularAluguel(aluguel)
+
+            if (aluguel.gamer.media > 8) {
+                valorAluguel -= valorAluguel * descontoReputacao
+            }
+
+            valorAluguel
         }
     }
 }
--- src/test/kotlin/org/game/api/data/modelo/GamerTest.kt
+++ src/test/kotlin/org/game/api/data/modelo/GamerTest.kt
@@ -26,19 +26,21 @@ fun main() {
 
     println(jogadorN1)
 
-    jogadorN2.plano = PlanoAssinatura("OURO", 59.90, 3)
+    jogadorN2.plano = PlanoAssinatura("OURO", 59.90, 3, 0.15)
 
     jogadorN2.jogos.add(jogos[5])
     jogadorN2.jogos.add(jogos[6])
     jogadorN2.jogos.add(jogos[7])
     jogadorN2.jogos.add(jogos[8])
 
+    jogadorN2.recomendar(10)
+
     jogadorN2.alugar(jogadorN2.jogos[0] ?: jogos[5], LocalDate.now(), LocalDate.now().plusDays(30))
     jogadorN2.alugar(jogadorN2.jogos[1] ?: jogos[6], LocalDate.now(), LocalDate.now().plusDays(60))
     jogadorN2.alugar(jogadorN2.jogos[2] ?: jogos[7], LocalDate.now(), LocalDate.now().plusDays(90))
     jogadorN2.alugar(jogadorN2.jogos[3] ?: jogos[8], LocalDate.now(), LocalDate.now().plusDays(90))
 
-    jogadorN2.recomendar(10)
+
 
     println(jogadorN2)
 

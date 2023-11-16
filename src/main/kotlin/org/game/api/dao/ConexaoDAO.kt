2b72d058cd91e86976af78598b99342ff0ddf92c
flavio.santana <9090flavio@gmail.com>
2023-11-16 18:21:04 -0300

    feat: Instalando o Hibernate

--- src/main/kotlin/org/game/api/dao/ConexaoDAO.kt
+++ src/main/kotlin/org/game/api/dao/ConexaoDAO.kt
@@ -1,10 +1,18 @@
 package org.game.api.dao
 
+import jakarta.persistence.EntityManager
+import jakarta.persistence.EntityManagerFactory
+import jakarta.persistence.Persistence
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
 object ConexaoDAO {
+
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("apigames")
+        return factory.createEntityManager()
+    }
 
     fun conectar(): Connection? {
         return try {
